c9fe70b4a694cd898cf481ddca90412c41d085f6
szuiderweg <svmzuiderweg@hotmail.com>
2023-11-26 22:32:24 +0100

    aanmaken project en de classes Main en Ingredient

---
+++ src/Ingredient.java
@@ -0,0 +1,17 @@
+public class Ingredient
+{
+    double amount;
+    double unit;
+    String name;
+
+    public Ingredient()//empty constructor
+    {}
+    public Ingredient(double amount, double unit, String name)//constructor with all 3 parameters
+    {
+        this.amount = amount;
+        this.unit = unit;
+        this.name = name;
+    }
+
+
+}
