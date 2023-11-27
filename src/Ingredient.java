cc9063e8aa944a821e088371290158e3b150cd61
szuiderweg <svmzuiderweg@hotmail.com>
2023-11-27 13:11:57 +0100

    1st working version of basic assignment

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,17 +1,50 @@
-public class Ingredient
+class Ingredient
 {
-    double amount;
-    double unit;
-    String name;
+    //todo how2 create getter and setter, zie H5.6 voor uitleg
+  //variabelen
+    private double amount;
+    private String unit;
+    private String name;
 
     public Ingredient()//empty constructor
     {}
-    public Ingredient(double amount, double unit, String name)//constructor with all 3 parameters
+    public Ingredient(double amount, String unit, String name)//constructor with all 3 parameters
     {
         this.amount = amount;
         this.unit = unit;
         this.name = name;
     }
+//amount of ingredient getter and setter
+    public double getAmount()
+    {
+        return amount;
+    }
 
+    public double setAmount(double amount)
+    {
+        return this.amount = amount;
+    }
+
+    //unit of ingredient getter and setter
+    public String getUnit()
+    {
+        return unit;
+    }
+
+    public String setUnit(String unit)
+    {
+        return this.unit = unit;
+    }
+
+    //name of ingredient getter and setter
+    public String getName()
+    {
+        return name;
+    }
+
+    public String setName(String name)
+    {
+        return this.name = name;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        var taart = new ApplePieRecipe();
+
+        taart.printIngredients();//print ingredients
+        //print recept
+        taart.preheatOven();
+        taart.beatEgg();
+        taart.createDough();
+        taart.makeFilling();
+        taart.prepareCakePan();
+        taart.formCrust();
+        taart.fillCake();
+        taart.cutDecoration();
+        taart.decorateCake();
+        taart.bakeCake();
+        //todo bonus assignment 1
+        //todo bonus assignment 2
     }
 }
